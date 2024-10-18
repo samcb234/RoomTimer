deb39ef5741fc1260f3f6b1375bfc4e666cf2410
samblock <block.sa@northeastern.edu>
2024-10-18 13:22:44 -0400

    updated rooms

--- drc-timer/src/utils/formatTime.ts
+++ drc-timer/src/utils/formatTime.ts
@@ -4,8 +4,9 @@ formatTime(timeLeft: number): string {
     const helper = (t: number): string =>{
         const hrs: number = Math.floor(t/3600)
         const min: number = Math.floor((t%3600)/60)
-        const sec: number = t%60
-        return `${hrs}:${min}:${String(sec).split('.')[0]}`
+        const secNum: number = t%60
+        const sec: string = secNum >= 10 ? String(secNum) : `0${secNum}`
+        return `${hrs}:${min}:${sec.split('.')[0]}`
     }
 
     if(timeLeft >= 0){
